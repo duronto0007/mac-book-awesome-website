7b584167315d41d68293e59026da09e53096622f
Your Name <satterhossain0@gmail.com>
2021-08-21 19:27:07 +0600

    finnal task completed

---
+++ assigenment.js
@@ -0,0 +1,89 @@
+/*memory price button*/
+const memoryBtn8 = document.getElementById('memory-8');
+const memoryBtn16 = document.getElementById('memory-16');
+/*storage price */
+const storageBtn256 = document.getElementById('storage-256')
+const storageBtn512 = document.getElementById('storage-512')
+const storageBtn1Tb = document.getElementById('storage-1tb')
+/* delivery charge button */ 
+const freeDelivery = document.getElementById('free');
+const expressDelivery = document.getElementById('express');
+
+const regularPrice = document.getElementById('regular-price');
+const extraMemory = document.getElementById('extra-memory');
+const extraStorage = document.getElementById('extra-storage');
+const deliveryCharge = document.getElementById('delivery-charge');
+const totalPrice = document.getElementById('total-price');
+const promoPrice = document.getElementById('promo-price');
+
+
+/********price calculate*************/
+function priceTotal(){
+    const regularPriceAmount = parseInt(regularPrice.innerText);
+    const extraMemoryAmount = parseInt(extraMemory.innerText);
+    const extraStorageAmount = parseInt(extraStorage.innerText);
+    const deliveryChargeAmount = parseInt(deliveryCharge.innerText);
+    const totalPriceAmount = regularPriceAmount + extraMemoryAmount + extraStorageAmount + deliveryChargeAmount;
+    return totalPriceAmount
+};
+
+/* Update Price */
+function updatePrice(){
+    const totalCalculate = priceTotal();
+    totalPrice.innerText = totalCalculate;
+    promoPrice.innerText = totalCalculate;
+}
+/* Memory button event  */
+memoryBtn8.addEventListener('click', function (){
+    extraMemory.innerText = 0;
+    updatePrice();
+});
+
+memoryBtn16.addEventListener('click', function (){
+    extraMemory.innerText = 180;
+    updatePrice();
+});
+
+/* storage button event */
+storageBtn256.addEventListener('click', function (){
+    extraStorage.innerText = 0;
+    updatePrice();
+});
+
+storageBtn512.addEventListener('click', function (){
+    extraStorage.innerText = 100;
+    updatePrice();
+});
+
+storageBtn1Tb.addEventListener('click', function (){
+    extraStorage.innerText = 180;
+    updatePrice();
+});
+
+/* Delivery charge button */
+freeDelivery.addEventListener('click', function (){
+    deliveryCharge.innerText = 0;
+    updatePrice();
+});
+
+expressDelivery.addEventListener('click', function (){
+    deliveryCharge.innerText = 20;
+    updatePrice();
+});
+
+/* Procode Handelar  */
+document.getElementById('promo-btn').addEventListener('click', function (){
+    const getTotalPrice = priceTotal();
+    const promoInput = document.getElementById('promo-input');
+    const promoInputValue = promoInput.value;
+    if (promoInputValue == 'stevekaku'){
+        const promoDiscount = getTotalPrice/5;
+        const discount = getTotalPrice - promoDiscount;
+        promoPrice.innerText = discount;
+
+    }
+    else{
+        promoPrice.innerText = getTotalPrice
+    };
+    promoInput.value = '';
+});
